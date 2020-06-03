f7d5c870a8ba74adefca776b2672873bd6139d36
tchsaro1 <roberto.salerno@swissscom.com>
2020-06-03 16:07:54 +0200

    logic

--- frontend/vanilla-frontend/calendermapper.js
+++ frontend/vanilla-frontend/calendermapper.js
@@ -3,79 +3,11 @@ export class Calendermapper {
 
     constructor() {
 
-        fetch('http://localhost:3000/month/' + 4)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-
-                appendData(data)
 
 
-            })
-            .catch(function (err) {
-                console.log('error: ' + err);
-            });
-
-
-        function appendData(data) {
-
-            const calenderMap = new Map();
-            let mainContainer = [];
-            for (let i = 0; i < data.length; i++) {
-                mainContainer.push(data[i]);
-                console.log(mainContainer)
-            }
-
-
-            let userMap = new Map();
-            for (let x = 0; x < mainContainer.length; x++){
-
-                let currentEntry = mainContainer[x];
-                let nextEntry = mainContainer[x + 1];
-
-
-                if ( nextEntry && currentEntry.eventdate === nextEntry.eventdate) {
-
-
-                    let user = currentEntry.firstname;
-                    let event = currentEntry.appointment;
-                    let eventsForUser = userMap.get(user);
-
-                    if (!eventsForUser){
-                        eventsForUser = [];
-                    }
-
-
-                        eventsForUser.push(event);
-                        userMap.set(user, eventsForUser)
-
-
-                }else {
-
-                    let user = currentEntry.firstname;
-                    let event = currentEntry.appointment;
-
-                    let eventsForUser = userMap.get(user);
-
-                    if (!eventsForUser){
-                        eventsForUser = [];
-                    }
-
-
-                    eventsForUser.push(event);
-                    userMap.set(user, eventsForUser)
-                    calenderMap.set(currentEntry.eventdate, userMap)
-
-                    userMap = new Map();
-
-                }
-            }
-            if (userMap.size > 0){
-                const key = mainContainer[mainContainer.length-1].eventdate;
-                calenderMap.set(key, userMap)
-
-            } console.log(calenderMap)
         }
-    }
+
+
+
+
 }
--- frontend/vanilla-frontend/view.js
+++ frontend/vanilla-frontend/view.js
@@ -1,9 +1,23 @@
 
-
 export class View {
 
 
     constructor() {
+
+        fetch('http://localhost:3000/month/' + 4)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+
+                appendData(data)
+
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+
+
 
 
 
@@ -15,7 +29,7 @@ export class View {
             let year = today.getFullYear();
 // body of the calendar
             let calendarBody = document.getElementById("days");
-            let user1 = document.getElementById("user1")
+     //       let user1 = document.getElementById("user1");
             let months = [
                 "Januar",
                 "Februar",
@@ -57,16 +71,12 @@ export class View {
                 let weekday = new Date(year, month).getDay();
                 // clearing all previous cells
                 calendarBody.innerHTML = "";
-                user1.innerText = "";
                 // checking the mount of days in this month to control the loop
                 let totalDays = daysInMonth(month, year);
                 // adding the dates to the calendar
                 for (let day = 1; day <= totalDays; day++) {
                     // create li node with text content & apend to body
                     let cell = document.createElement("li");
-                    let user = document.createElement("li");
-                    user.setAttribute("id", day + 1 -1)
-                    user.setAttribute("user", "Roberto")
 
                         let cellText = document.createTextNode(weekdays[weekday] + " " + day + ". " + months[month]);
                         if (weekday > 5) {
@@ -92,7 +102,7 @@ export class View {
                     cell.appendChild(cellText);
 
                     calendarBody.appendChild(cell);
-                    user1.appendChild(user)
+
                 }
                 // set month string value
                 document.getElementById("month").innerHTML = months[month];
@@ -112,10 +122,99 @@ export class View {
             }
 
 
-function previous() {
-            this.year = month === 0 ? year - 1 : year;
-            this.monthmonth = month === 0 ? 11 : month - 1;
+            function previous() {
+            year = month === 0 ? year - 1 : year;
+            month = month === 0 ? 11 : month - 1;
             showCalendar(month, year);
+        }
+
+        function appendData(data) {
+
+            const calenderMap = new Map(); // neue Haupt-Map initialisiert
+            let mainContainer = [];
+            for (let i = 0; i < data.length; i++) {
+                mainContainer.push(data[i]);
+            }
+
+            let userMap = new Map(); // neue Map initialisiert für ein user mit event
+            for (let x = 0; x < mainContainer.length; x++){
+
+                let currentEntry = mainContainer[x]; //aktueller Eintrag
+                let nextEntry = mainContainer[x + 1]; // nächster Eintrag
+
+                if (nextEntry && currentEntry.eventdate === nextEntry.eventdate) { //check ob es weitere Events am gleichen Tag gibt
+
+                    let user = currentEntry.firstname;
+                    let event = currentEntry.appointment;
+
+                    let eventsForUser = userMap.get(user); //in erster Iteration undefined
+
+                    if (!eventsForUser){
+                        eventsForUser = [];  // falls undefinend erstelle neues leeres Array
+                    }
+
+                    eventsForUser.push(event);  // füge event dem array hinzu
+                    userMap.set(user, eventsForUser); // füge alle events für einen User hinzu. (key und value)
+                }
+
+                else {  // falls nächstes Datum unterschiedlich ist => neue map erstellen
+
+                    let user = currentEntry.firstname;
+                    let event = currentEntry.appointment;
+
+                    let eventsForUser = userMap.get(user);
+
+                    if (!eventsForUser){
+                        eventsForUser = [];
+                    }
+
+                    eventsForUser.push(event);
+                    userMap.set(user, eventsForUser);
+
+                    calenderMap.set(currentEntry.eventdate, userMap); // setze key und value für haupt-map
+
+                    userMap = new Map(); // neue userMap für neues Datum wird initialisiert.
+                }
+            }
+            if (userMap.size > 0){          // bearbeitung des letzten Eintrags
+                const key = mainContainer[mainContainer.length-1].eventdate;
+                calenderMap.set(key, userMap)
+            }
+
+
+            let row1 = document.getElementById("user1");
+            let row2 = document.getElementById("user2");
+
+            for (let [key, value] of calenderMap) {
+
+                let r = "Roberto";
+                let s = "Sebi";
+                let a = [];
+                let b = [];
+                let xy = value.entries();
+                for (let [user, termin] of xy){
+                    if (user === r){
+                        a.push(user, termin)
+                    }
+
+                    if(user === s){
+                        b.push(user, termin)
+                    }
+                }
+                console.log(key, value)
+
+                let user1 = document.createElement("li");
+                user1.innerHTML = key + "<br />" + a;
+                user1.setAttribute("user", "Roberto");
+                row1.appendChild(user1);
+
+                let user2 = document.createElement("li");
+                user2.innerHTML = key + "<br />" + b;
+                user2.setAttribute("user", "Sebi");
+                row2.appendChild(user2);
+
+
+            }
         }
     }
 }
