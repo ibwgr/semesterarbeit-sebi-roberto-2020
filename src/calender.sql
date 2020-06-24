52d29d9eda86efa56f28744e7a3b5470da4c33e6
tchsaro1 <roberto.salerno@swissscom.com>
2020-06-24 22:34:55 +0200

    db-script-optimiert

--- src/calender.sql
+++ src/calender.sql
@@ -16,17 +16,19 @@ INSERT into calender.familycalender
 
 VALUES
 
-('Roberto', 'Feuerwehr', '2020-06-05'),
-('Roberto', 'Frei', '2020-06-05'),
-('Sebi', 'Feuerwehr', '2020-04-05'),
-('Sebi', 'Turnen', '2020-03-05'),
-('Susi', 'Singen','2020-08-01'),
-('Roberto', 'Surfen','2020-08-01'),
-('Fredi', 'Ferien','2020-06-09'),
-('Roberto', 'Einkaufen', '2020-06-17'),
-('Sebi', 'Tennis', '2020-03-15'),
-('Susi', 'Kino','2020-04-11'),
-('Roberto', 'Arbeit','2020-04-11'),
-('Fredi', 'Frei','2020-06-24'),
-('Roberto', 'Frei','2020-06-25'),
-('Sebi', 'Ausgang','2020-06-25')
+('Vater', 'Feuerwehr 19 Uhr', '2020-06-05'),
+('Vater', 'Frei', '2020-06-05'),
+('Mutter', 'Feuerwehr 19 Uhr', '2020-04-05'),
+('Mutter', 'Turnen 20 Uhr', '2020-03-05'),
+('Sohn', 'Singen 14 Uhr','2020-08-01'),
+('Vater', 'Unihockey 21 Uhr','2020-08-01'),
+('Tochter', 'Geburtstag','2020-06-09'),
+('Vater', 'Sitzung 18 Uhr', '2020-06-17'),
+('Mutter', 'Tennis 16 Uhr', '2020-03-15'),
+('Sohn', 'Kino 22 Uhr','2020-04-11'),
+('Vater', 'Ferien','2020-04-11'),
+('Tochter', 'Reiten 12 Uhr','2020-06-24'),
+('Vater', 'Frei','2020-06-25'),
+('Mutter', 'Banktermin 11 Uhr','2020-06-25')
+('Vater', 'Wellness','2020-06-06'),
+('Mutter', 'Wellness','2020-06-06')
